948de597bdc7e26aada0511475c89d2d8c397aa1
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-12-20 18:30:57 -0800

    install vite and dependencies

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  tailwindFunctions: ['clsx'],
+  organizeImportsSkipDestructiveCodeActions: true,
+}
