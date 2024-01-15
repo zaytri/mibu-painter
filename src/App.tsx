0ca6d5403039c1bd352bd201199b1752e77bd258
Zaytri <9072119+zaytri@users.noreply.github.com>
2024-01-15 01:41:43 -0800

    hotbar start

--- src/App.tsx
+++ src/App.tsx
@@ -49,52 +49,92 @@ export default function App() {
   }, [])
 
   return (
-    <div
-      className={clsx(
-        'checkerboard flex h-full w-full border-4 border-neutral-500 bg-neutral-500 p-2',
-        direction === 'horizontal' && 'flex-row',
-        direction === 'vertical' && 'flex-col',
-      )}
-      // onClick={draw}
-      // onPointerDown={onPointerDown}
-      // onPointerUp={onPointerUp}
-      // onPointerMove={onPointerMove}
-    >
-      <div
-        className='resizable relative min-h-48 min-w-48 cursor-crosshair'
-        style={{ '--customFlex': flex } as React.CSSProperties}
-      >
-        <Scene />
-      </div>
+    <div className='flex h-full w-full'>
       <div
         className={clsx(
-          'flex touch-none items-stretch justify-center p-2 active:cursor-grabbing',
-          direction === 'horizontal' && 'h-full cursor-col-resize flex-row',
-          direction === 'vertical' && 'w-full cursor-row-resize flex-col',
+          'checkerboard relative flex flex-1 border-4 border-neutral-500 bg-white/5 p-2',
+          direction === 'horizontal' && 'flex-row',
+          direction === 'vertical' && 'flex-col',
         )}
-        {...bind()}
+        onClick={draw}
+        onPointerDown={onPointerDown}
+        onPointerUp={onPointerUp}
+        onPointerMove={onPointerMove}
       >
         <div
-          className={clsx(
-            '-mx-4 -my-4 bg-neutral-500',
-            direction === 'horizontal' && 'w-1',
-            direction === 'vertical' && 'h-1',
-          )}
-        ></div>
-      </div>
-      <div
-        className={clsx(
-          'resizable relative flex min-h-48 min-w-48',
-          direction === 'horizontal' && 'flex-col',
-          direction === 'vertical' && 'flex-row',
-        )}
-        style={{ '--customFlex': 1 - flex } as React.CSSProperties}
-      >
-        <div className='min-h-48 min-w-48 flex-1 cursor-crosshair'>
-          <Preview />
+          className='resizable relative min-h-48 min-w-48 cursor-crosshair'
+          style={{ '--customFlex': flex } as React.CSSProperties}
+        >
+          <Scene />
         </div>
-        <div className='hidden min-h-48 min-w-48 flex-1 bg-rose-300'></div>
+        <div
+          className={clsx(
+            'flex touch-none items-stretch justify-center p-2 active:cursor-grabbing',
+            direction === 'horizontal' && 'h-full cursor-col-resize flex-row',
+            direction === 'vertical' && 'w-full cursor-row-resize flex-col',
+          )}
+          {...bind()}
+        >
+          <div
+            className={clsx(
+              '-mx-4 -my-4 bg-neutral-500',
+              direction === 'horizontal' && 'w-1',
+              direction === 'vertical' && 'h-1',
+            )}
+          ></div>
+        </div>
+        <div
+          className={clsx(
+            'resizable relative flex min-h-48 min-w-48',
+            direction === 'horizontal' && 'flex-col',
+            direction === 'vertical' && 'flex-row',
+          )}
+          style={{ '--customFlex': 1 - flex } as React.CSSProperties}
+        >
+          <div className='min-h-48 min-w-48 flex-1 cursor-crosshair'>
+            <Preview />
+          </div>
+          <div className='hidden min-h-48 min-w-48 flex-1 bg-rose-300'></div>
+        </div>
+        <div className='absolute bottom-10 left-1/2 flex -translate-x-1/2'>
+          <div className='mr-8 overflow-hidden rounded-lg border-[3px] border-black'>
+            <div className='hotcell'></div>
+          </div>
+          <div className='flex border-[3px] border-black'>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+            <div className='hotcell'>
+              <div className='hotcell-select'></div>
+            </div>
+          </div>
+        </div>
       </div>
+      <div className='w-72 border-4 border-l-0 border-neutral-500 bg-neutral-900'></div>
     </div>
   )
 }
